5beba097d4c20ae27945992d3d25b05cbf645047
htunhtunhtet <ryanhhh91@gmail.com>
2022-11-01 01:40:10 +0800

    add pagination

--- src/util/api.tsx
+++ src/util/api.tsx
@@ -1,7 +1,7 @@
 const root = `https://pokeapi.co/api/v2`;
 
-export function getAllPokemons() {
-    return fetch(`${root}/pokemon/`)
+export function getAllPokemons(offset =0 , limit = 20) {
+    return fetch(`${root}/pokemon/?offset=${offset}&limit=${limit}`)
         .then(result => result.json())
         .then(result => result)
 }
